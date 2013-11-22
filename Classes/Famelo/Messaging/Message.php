dd211796f5d4968aa9ef1af91259cb869d9a5861
Marc Neuhaus <mneuhaus@famelo.com>
2013-11-22 12:21:05 +0100

    [FEATURE] Added option to render template from String

--- Classes/Famelo/Messaging/Message.php
+++ Classes/Famelo/Messaging/Message.php
@@ -35,6 +35,11 @@ class Message extends \TYPO3\SwiftMailer\Message {
 	protected $package = NULL;
 
 	/**
+	 * @var string
+	 */
+	protected $source = NULL;
+
+	/**
 	 * @Flow\Inject
 	 * @var \TYPO3\Flow\Configuration\ConfigurationManager
 	 */
@@ -85,6 +90,7 @@ class Message extends \TYPO3\SwiftMailer\Message {
 			$routesConfiguration = $this->configurationManager->getConfiguration(\TYPO3\Flow\Configuration\ConfigurationManager::CONFIGURATION_TYPE_ROUTES);
 			$this->router->setRoutesConfiguration($routesConfiguration);
 			self::$routerConfigured = TRUE;
+			putenv('REDIRECT_FLOW_REWRITEURLS=true');
 		}
 
 		$this->setBody($this->render(), $this->getContentType());
@@ -92,19 +98,25 @@ class Message extends \TYPO3\SwiftMailer\Message {
 	}
 
 	public function render() {
-		if ($this->package === NULL) {
-			$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-			$class = $trace[1]['class'];
-			preg_match('/([A-Za-z]*)\\\\([A-Za-z]*)/', $class, $match);
-			$this->package = $match[1] . '.' . $match[2];
-		}
+		$this->view->getRequest()->getHttpRequest()->injectSettings($this->configurationManager->getConfiguration(\TYPO3\Flow\Configuration\ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'TYPO3.Flow'));
 
-		$replacements = array(
-			'@package' => $this->package,
-			'@message' => $this->message
-		);
-		$template = str_replace(array_keys($replacements), array_values($replacements), $this->templatePath);
-		$this->view->setTemplatePathAndFilename($template);
+		if ($this->source === NULL) {
+			if ($this->package === NULL) {
+				$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+				$class = $trace[1]['class'];
+				preg_match('/([A-Za-z]*)\\\\([A-Za-z]*)/', $class, $match);
+				$this->package = $match[1] . '.' . $match[2];
+			}
+
+			$replacements = array(
+				'@package' => $this->package,
+				'@message' => $this->message
+			);
+			$template = str_replace(array_keys($replacements), array_values($replacements), $this->templatePath);
+			$this->view->setTemplatePathAndFilename($template);
+		} else {
+			$this->view->setTemplateSource($this->source);
+		}
 		return $this->view->render();
 	}
 
@@ -117,6 +129,11 @@ class Message extends \TYPO3\SwiftMailer\Message {
 		foreach ($values as $key => $value) {
 			$this->assign($key, $value);
 		}
+		return $this;
+	}
+
+	public function setTemplateSource($source) {
+		$this->source = $source;
 		return $this;
 	}
 }
